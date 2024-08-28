a32412ba37461e061249a7c86fb6bcfab64341ca
Xylo-o <tuadrian1327@gmail.com>
2024-08-28 16:18:56 -0400

    Added feature for only one argument, exported printing loop to a function, added loop to iterate through multiple arguments

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,30 +6,43 @@
 /*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 13:22:27 by kali              #+#    #+#             */
-/*   Updated: 2024/08/28 13:50:35 by kali             ###   ########.fr       */
+/*   Updated: 2024/08/28 16:17:13 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+void    print_str(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        char c = str[i];
+        if (c > 96 && c < 123)
+            c -= 32;
+        std::cout << c;
+        i++;
+    }
+}
+
 int main(int argc, char **argv)
 {
-    if (argc < 2)
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    else if (argc < 2)
     {
         std::cout << "Invalid number of arguments" << std::endl;
         return (1);
     }
-    char *str;
-    str = argv[1];
-    int i = 0;
-    while (str[i])
+    int i = 1;
+    while (i < argc)
     {
-        if (str[i] > 96 && str[i] < 123)
-        {
-            str[i] -= 32;
-        }
+        print_str(argv[i]);
+        std::cout << std::endl;
         i++;
     }
-    std::cout << str << std::endl;
     return (0);
 }
