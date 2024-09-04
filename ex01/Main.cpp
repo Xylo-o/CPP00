13d3988e26de996eab58395b14068542f4220a65
Xyloo <adprzyby@student.42heilbronn.de>
2024-09-04 11:04:39 +0200

    Setting up files

---
+++ ex01/Main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 10:58:20 by adprzyby          #+#    #+#             */
+/*   Updated: 2024/09/04 10:58:36 by adprzyby         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string name;
+	int phoneNumber;
+	std::getline(std::cin, name);
+	std::getline(std::cin, phoneNumber;
+
+	
+	
+	return (0);
+}
