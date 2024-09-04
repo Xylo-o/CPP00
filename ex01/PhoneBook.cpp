e0417126db619d9e76fd0158b7426a53340a96a7
Xyloo <adprzyby@student.42heilbronn.de>
2024-09-04 12:21:57 +0200

    Building phone book

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,11 +6,30 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 09:35:58 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/09/04 10:59:37 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/09/04 12:20:37 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 class PhoneBook
 {
+	int contacts[8];
 	
 };
+
+void	addContact(void)
+{
+	int number;
+	std::string name;
+	std::string address;
+	
+	std::cin >> number;
+	std::getline(std::cin, name);
+	std::getline(std::cin, address);
+}
+
+void	searchContact(void)
+{
+		
+}
