8d39ba2b9d56369280f9bfd06d01583263e68d7a
Xyloo <adprzyby@student.42heilbronn.de>
2024-09-04 11:57:25 +0200

    Added command and a loop in main to check if command is valid

--- ex01/Main.cpp
+++ ex01/Main.cpp
@@ -6,41 +6,51 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 10:58:20 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/09/04 11:25:37 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/09/04 11:54:35 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void checkIfCommand(std::string word)
+int checkIfCommand(std::string word)
 {
-	
+	if (word == "ADD")
+	{
+		std::cout << "Adding\n";
+		return (1);
+	}
+	else if (word == "SEARCH")
+	{
+		std::cout << "Searching... \n";
+		return (1);
+	}
+	else if (word == "EXIT")
+	{
+		std::cout << "Exiting ... \n";
+		return (0);
+	}
+	else
+		return (-1);
 }
 
 int main(void)
 {
-	std::string command;
-
+	std::string input;
+	int isCommand = 0;
 	std::cout << "Welcome to myPhoneBook! Please enter one of the following commands: \n"
 			  << "ADD - save a new contact\n" 
 			  << "SEARCH - display a specific contact\n" 
-			  << "EXIT - exit the program" << std::endl;
-	
-	std::getline(std::cin, command);
-	
-	if (command == "ADD")
-	{
-		
-	}
-	else if (command == "SEARCH")
-	{
-		
-	}
-	else if (command == "EXIT")
-	{
+			  << "EXIT - exit the program and delete all contacts" << std::endl;
 
+	while (std::getline(std::cin, input))
+	{
+		isCommand = checkIfCommand(input);
+		if (isCommand == -1)
+			std::cout << "Command not found, please try again" << std::endl;
+		if (isCommand == 0)
+			return (0);
 	}
-	else
-		std::cout << "Command not found, please try again" << std::endl;
+	
+
 	return (0);
 }
